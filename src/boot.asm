ea4c8e0fdfd56f52c5e3c2a39eff8397e45b0b84
Soham Metha <sohammetha01@gmail.com>
2025-04-28 22:49:09 +0000

    29/04/2025 : 04:19:09

--- src/boot.asm
+++ src/boot.asm
@@ -1,8 +1,35 @@
-section .text
+section .data
+    hlo: db "Test?",0
+
+section .code
 
 .global _start
 
 _start:
-    LJMP 0x07C0, main
+    LJMP 0x07C0, $main
 
 main:
+    CLI
+    MOV AX, CS      ; On Startup, CS is 0xF000, IP is 0xFFF0
+    MOV DS, AX
+    MOV ES, AX
+    MOV SS, AX
+    MOV SP, 0x7C00
+    STI
+    MOV SI, hlo
+
+printstr:
+
+    LODSB
+    CMP AL,0
+    JE  exit
+    MOV AH, 0x0E
+    INT 10H
+    JMP printstr
+
+exit:
+    CLI
+    HLT
+
+    TIMES 510-($$-$) DB 0
+    DW 0xAA55
