a19f557376cab8a684dcfdd121c1f6fdc74f2e0e
Soham Metha <sohammetha01@gmail.com>
2025-04-29 04:12:33 +0000

    29/04/2025 : 09:42:33

--- src/kernel.c
+++ src/kernel.c
@@ -1,19 +1,19 @@
 #include "kernel.h"
 
 extern void kernel_main(){
-    char* display = (char*) 0xb8000;
-    display[0] = 'H'; display[1] = 0x07;
-    display[2] = 'e'; display[3] = 0x07;
-    display[4] = 'l'; display[5] = 0x07;
-    display[6] = 'l'; display[7] = 0x07;
-    display[8] = 'o'; display[9] = 0x07;
-    display[10] = ' '; display[11] = 0x07;
-    display[12] = 'W'; display[13] = 0x07;
-    display[14] = 'o'; display[15] = 0x07;
-    display[16] = 'r'; display[17] = 0x07;
-    display[18] = 'l'; display[19] = 0x07;
-    display[20] = 'd'; display[21] = 0x07;
-    display[22] = '!'; display[23] = 0x07;
+    char* vga = (char*) 0xb8000;
+    vga[0] = 'H'; vga[1] = 0x07;
+    vga[2] = 'e'; vga[3] = 0x07;
+    vga[4] = 'l'; vga[5] = 0x07;
+    vga[6] = 'l'; vga[7] = 0x07;
+    vga[8] = 'o'; vga[9] = 0x07;
+    vga[10] = ' '; vga[11] = 0x07;
+    vga[12] = 'W'; vga[13] = 0x07;
+    vga[14] = 'o'; vga[15] = 0x07;
+    vga[16] = 'r'; vga[17] = 0x07;
+    vga[18] = 'l'; vga[19] = 0x07;
+    vga[20] = 'd'; vga[21] = 0x07;
+    vga[22] = '!'; vga[23] = 0x07;
 
     return;
 }
