ea00c0bd90e07749d3bcdebeac0366f8894da8a6
Soham Metha <sohammetha01@gmail.com>
2025-04-29 03:38:09 +0000

    29/04/2025 : 09:08:09

--- src/boot.asm
+++ src/boot.asm
@@ -4,8 +4,7 @@ section .data
 dataStart:
     ;KERNEL_BASE  equ 0x1000      ; segment base address for kernel
     ;KERNEL_START equ     ; expected position of kernel in the memory (kernel will be placed at this position after linking
-    DISK_ID db 0
-    padding db 0
+    DISK_ID db 1
 
     gdtStart:
         dd 0
