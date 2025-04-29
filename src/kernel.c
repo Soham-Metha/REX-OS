7ba38887c6b42dcca11d180ddc341a02c64a1c35
Soham Metha <sohammetha01@gmail.com>
2025-04-29 05:53:10 +0000

    29/04/2025 : 11:23:10

--- src/kernel.c
+++ src/kernel.c
@@ -2,9 +2,11 @@
 extern void kernel_main() {
     
     char* str = "Hello, Kernel!";
+    char* vga = (char*)0xB8000;
     while (*str) {
-        *(char*)0xB8000 = *str | 0x0F00;
+        *vga = *str | 0x0F00;
         str++;
+        vga+=2;
     }
 
     while (1) {
