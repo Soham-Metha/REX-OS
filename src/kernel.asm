433b0b25a6a5dd78aa53cf15caf4f4ba745fc0a7
Soham Metha <sohammetha01@gmail.com>
2025-04-29 04:39:04 +0000

    29/04/2025 : 10:09:04

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,12 +1,8 @@
-[BITS 32]
-
-global _start
-
-_start:
+section .text
     mov al, 'Z'
     mov ah, 0x0f
     mov [0xb8000], ax
     [BITS 32]
     [extern kernel_main]
     call kernel_main
-    HLT
+    jmp $
