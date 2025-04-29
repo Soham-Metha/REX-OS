166ddab0af71d46a52661c58d7770acd1de7322d
Soham Metha <sohammetha01@gmail.com>
2025-04-29 04:11:21 +0000

    29/04/2025 : 09:41:21

--- src/kernel.c
+++ src/kernel.c
@@ -1,19 +1,19 @@
 #include "kernel.h"
 
 extern void kernel_main(){
-    
-    *(char*)0xb8000 = 'H';
-    *(char*)0xb8001 = 'e';
-    *(char*)0xb8002 = 'l';
-    *(char*)0xb8003 = 'l';
-    *(char*)0xb8004 = 'o';
-    *(char*)0xb8005 = ' ';
-    *(char*)0xb8006 = 'W';
-    *(char*)0xb8007 = 'o';
-    *(char*)0xb8008 = 'r';
-    *(char*)0xb8009 = 'l';
-    *(char*)0xb800A = 'd';
-    *(char*)0xb800B = '!';
+    char* display = 0xb8000;
+    display[0] = 'H'; display[1] = 0x07;
+    display[2] = 'e'; display[3] = 0x07;
+    display[4] = 'l'; display[5] = 0x07;
+    display[6] = 'l'; display[7] = 0x07;
+    display[8] = 'o'; display[9] = 0x07;
+    display[10] = ' '; display[11] = 0x07;
+    display[12] = 'W'; display[13] = 0x07;
+    display[14] = 'o'; display[15] = 0x07;
+    display[16] = 'r'; display[17] = 0x07;
+    display[18] = 'l'; display[19] = 0x07;
+    display[20] = 'd'; display[21] = 0x07;
+    display[22] = '!'; display[23] = 0x07;
 
     return;
 }
