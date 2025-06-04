0faf681a7cf1c4d49da3cd90c01c1ffc1df7fa46
Soham Metha <sohammetha01@gmail.com>
2025-06-04 06:00:38 +0000

    04/06/2025 : 11:30:38

--- GNU_rerwite_src/boot.s
+++ GNU_rerwite_src/boot.s
@@ -1,6 +1,9 @@
 [BITS 32]
 
 section .text
+    ; MULTIBOOT HEADER of size 8 bytes
     ALIGN   4
     DD      0x1BADB002
-    
+    DD      0x00000000
+    ; checksum
+    DD      -( 0x1BADB002 + 0x00000000)
