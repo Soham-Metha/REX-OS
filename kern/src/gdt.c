d382ca10ab2bc6cc0c139b0767833c8ff23710fa
Soham Metha <sohammetha01@gmail.com>
2025-06-04 12:28:55 +0000

    04/06/2025 : 17:58:55

--- kern/src/gdt.c
+++ kern/src/gdt.c
@@ -1,4 +1,13 @@
 #include "gdt.h";
 
-void initGdt();
+extern void gdt(addr_t);
+
+Descriptor gdtEntries[5];
+GdtPtr gdtPtr;
+
+void initGdt()
+{
+    gdtPtr.limit = (sizeof(Descriptor)*5)-1;
+    gdtPtr.base = &gdtEntries;
+}
 void setGdtEntry(uint32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags);
