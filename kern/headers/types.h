3922d2b2721020ff54f5271af67b16729c211941
Soham Metha <sohammetha01@gmail.com>
2025-06-04 08:34:43 +0000

    04/06/2025 : 14:04:43

--- kern/headers/types.h
+++ kern/headers/types.h
@@ -4,3 +4,11 @@ typedef signed char int1;
 typedef signed short int2;
 typedef signed long int int4;
 typedef signed long long int int8;
+
+typedef unsigned char uint1;
+typedef unsigned short uint2;
+typedef unsigned long int uint4;
+typedef unsigned long long int uint8;
+
+#define true 1
+#define false 0
