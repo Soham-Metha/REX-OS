d5c73504ce49473cb18577b5963b579fcb4b30ad
Soham Metha <sohammetha01@gmail.com>
2025-06-04 06:57:58 +0000

    04/06/2025 : 12:27:58

--- GNU_rewrite_src/src/kernel.c
+++ GNU_rewrite_src/src/kernel.c
@@ -1,17 +1,5 @@
 #include <kernel.h>
 
-void test2(){
-
-    char* str = "Dont print this2222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222!";
-    char* vga = (char*)0xB8000;
-    while (*str) {
-        *vga = *str | 0x0F00;
-        str++;
-        vga+=2;
-    }
-
-}
-
 extern void kernelMain() {
     
     char* str = "Hello, Kernel!";
