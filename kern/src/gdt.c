aa31032f0e44e36869959b493445b0c1c7e3e5f2
Soham Metha <sohammetha01@gmail.com>
2025-06-04 12:31:35 +0000

    04/06/2025 : 18:01:35

--- kern/src/gdt.c
+++ kern/src/gdt.c
@@ -7,7 +7,13 @@ GdtPtr gdtPtr;
 
 void initGdt()
 {
-    gdtPtr.limit = (sizeof(Descriptor)*5)-1;
+    gdtPtr.limit = (sizeof(Descriptor) * 5) - 1;
     gdtPtr.base = &gdtEntries;
+
+    setGdtEntry(0, 0, 0, 0, 0);
+    setGdtEntry(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
+    setGdtEntry(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
+    setGdtEntry(3, 0, 0xFFFFFFFF, 0xFA, 0xCF);
+    setGdtEntry(4, 0, 0xFFFFFFFF, 0xF2, 0xCF);
 }
 void setGdtEntry(uint32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags);
