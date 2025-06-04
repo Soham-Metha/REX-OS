497d4437736dae19d8404e85a9b98b423e889b3d
Soham Metha <sohammetha01@gmail.com>
2025-06-04 10:44:04 +0000

    04/06/2025 : 16:14:04

--- kern/headers/descriptors.h
+++ kern/headers/descriptors.h
@@ -1,5 +1,10 @@
 #include "types.h"
 
 typedef struct descriptor {
-
-} Descriptor;
+    uint16_t limit_0_15;
+    uint16_t base_0_15;
+    uint8_t base_16_23;
+    uint8_t AccessByte;
+    uint8_t limit_16_19_flags;
+    uint8_t base_24_31;
+} __attribute__((packed)) Descriptor;
