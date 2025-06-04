a617289e00ed9427127b066b5cf72c743e49d4a9
Soham Metha <sohammetha01@gmail.com>
2025-06-04 12:19:52 +0000

    04/06/2025 : 17:49:52

---
+++ kern/headers/gdt.h
@@ -0,0 +1,16 @@
+#include "types.h"
+
+typedef struct descriptor
+{
+    uint16_t limit_0_15;
+    uint16_t base_0_15;
+    uint8_t base_16_23;
+    uint8_t AccessByte;
+    uint8_t limit_16_19_flags;
+    uint8_t base_24_31;
+} __attribute__((packed)) Descriptor;
+
+typedef struct gdtPtr {
+    uint16_t limit;
+    unsigned int base;
+} GdtPtr;
