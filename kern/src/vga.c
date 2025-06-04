f2c9d7c91668241108b96791c70c334763909c50
Soham Metha <sohammetha01@gmail.com>
2025-06-04 09:07:40 +0000

    04/06/2025 : 14:37:40

--- kern/src/vga.c
+++ kern/src/vga.c
@@ -1,6 +1,27 @@
 #include "vga.h"
+#include "types.h"
 
+uint16_t *vga = (uint16_t *)0xB8000;
+uint16_t column = 0;
+uint16_t row = 0;
+const uint16_t defaultColor = (COLOR8_BLACK << 8) | (COLOR8_LIGHT_GREY << 12);
+uint16_t currentColor = defaultColor;
 
-void print(const char* ){
-    
+void clearScreen()
+{
+    column = 0;
+    row = 0;
+    currentColor = defaultColor;
+
+    for (uint16_t y = 0; y < HEIGHT; y++)
+    {
+        for (uint16_t x = 0; x < WIDTH; x++)
+        {
+            vga[y * WIDTH + x] = ' ' | currentColor;
+        }
+    }
+}
+
+void print(const char *)
+{
 }
