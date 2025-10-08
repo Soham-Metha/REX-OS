f54f5077f1315590b3311ee5bc0b6f87c339de3f
Soham Metha <sohammetha01@gmail.com>
2025-10-08 18:38:36 +0530

    re-interpret vga as a 2d array

--- src/kern/vga.c
+++ src/kern/vga.c
@@ -1,19 +1,24 @@
 #include "vga.h"
 #include "types.h"
 
-uint16_t col = 0;
-uint16_t row = 0;
-volatile uint16_t *const vga =
-    (uint16_t *const)0xB8000; // uint *const = constant pointer, const uint16_t* = pointer pointing to const value
-const uint16_t defaultColor = (COLOR8_BLACK << 12) | (COLOR8_LIGHT_GREY << 8);
-uint16_t currentColor = defaultColor;
+uint16_t col                          = 0;
+uint16_t row                          = 0;
+volatile uint16_t (*const vga)[WIDTH] = (volatile uint16_t (*)[WIDTH])0xB8000;     // uint *const = constant pointer, const uint16_t* = pointer pointing to const value
+const uint16_t defaultColor           = (COLOR8_BLACK << 12) | (COLOR8_LIGHT_GREY << 8);
+uint16_t currentColor                 = defaultColor;
 
-#define writeChar(ch)                                                                                                  \
-    {                                                                                                                  \
-        if (col == WIDTH)                                                                                              \
-            newLine();                                                                                                 \
-        vga[row * WIDTH + (col++)] = ch | currentColor;                                                                \
+#define writeChar(ch)                        \
+    {                                        \
+        if (col == WIDTH)                    \
+            newLine();                       \
+        vga[row][col++] = currentColor | ch; \
     }
+
+void row_clear(int row)
+{
+    for (uint16_t x = 0; x < WIDTH; x++)
+        vga[row][x] = currentColor | ' ';
+}
 
 void clearScreen()
 {
@@ -21,8 +26,7 @@ void clearScreen()
     row = 0;
 
     for (uint16_t y = 0; y < HEIGHT; y++)
-        for (uint16_t x = 0; x < WIDTH; x++)
-            vga[y * WIDTH + x] = currentColor | ' ';
+        row_clear(y);
 }
 
 void newLine()
@@ -38,18 +42,15 @@ void scrollUp()
 {
     for (uint16_t y = 1; y < HEIGHT; y++)
         for (uint16_t x = 0; x < WIDTH; x++)
-            vga[(y - 1) * WIDTH + x] = vga[y * WIDTH + x];
+            vga[y - 1][x] = vga[y][x];
 
-    for (uint16_t x = 0; x < WIDTH; x++)
-        vga[(HEIGHT - 1) * WIDTH + x] = currentColor | ' ';
+    row_clear(HEIGHT - 1);
 }
 
-void print(const char *s)
+void print(const char* s)
 {
-    while (*s)
-    {
-        switch (*s)
-        {
+    while (*s) {
+        switch (*s) {
         case '\n':
             newLine();
             break;
@@ -58,7 +59,7 @@ void print(const char *s)
             break;
         case '\t':
             int tabLen = 4 - (col % 4);
-            while (tabLen)
+            while (tabLen--)
                 writeChar(' ');
             break;
         default:
@@ -73,6 +74,7 @@ void setBG(Color col)
 {
     currentColor = ((currentColor & ~(0x0F << 12)) | (((col) & 0x0F) << 12));
 }
+
 void setFG(Color col)
 {
     currentColor = ((currentColor & ~(0x0F << 8)) | (((col) & 0x0F) << 8));
